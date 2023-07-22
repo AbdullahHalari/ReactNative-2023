34b2f130ad8b994db874ca9ca789bbabf5fb206c
AbdullahHalari <droidabdullah@gmail.com>
2023-07-23 01:16:17 +0500

    auth,auth-context,store_token

---
+++ user authentication/screens/WelcomeScreen.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../store/auth-context';
+function WelcomeScreen() {
+  const [message,setMessage] = useState('');
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  useEffect(()=>{
+    axios.get("https://olxbyabdullah.firebaseio.com/message.json?auth="+token
+    ).then((response)=>{
+      console.log(response.data)
+      setMessage(response.data)
+    })
+  },[token])
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
